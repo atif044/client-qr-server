5d434e9b52ffa22a82510dd2e9b86739ea756880
atif044 <74341827+atif044@users.noreply.github.com>
2023-12-18 01:37:22 +0500

    changes

--- database/db.js
+++ database/db.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2');
-
+require('dotenv').config();
 const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'client-qrcode',
+  host: process.env.HOST,
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
