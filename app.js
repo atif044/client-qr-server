4e970c717fa68b92b7cb76445d3dd887aeb229eb
atif044 <74341827+atif044@users.noreply.github.com>
2023-12-21 19:32:32 +0500

    meaowwvdxxv

--- app.js
+++ app.js
@@ -15,7 +15,12 @@ app.use(
 );
 // Server side rendering react app
 app.use(express.static(path.join(__dirname,'./build')));
-app.get("/*"
+app.get("/"
+, (req, res) => {
+  const indexHtml = fs.readFileSync(path.join(__dirname, './build/index.html'), 'utf8');
+  res.send(indexHtml);
+});
+app.get(/^(?!\/api\b).*|^\/?$/
 , (req, res) => {
   const indexHtml = fs.readFileSync(path.join(__dirname, './build/index.html'), 'utf8');
   res.send(indexHtml);
