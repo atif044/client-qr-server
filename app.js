ebc6fd54a2bb0cd1f18dcc6fad78a28b9ad2e0b2
atif044 <74341827+atif044@users.noreply.github.com>
2023-12-21 20:00:06 +0500

    meaowwvdxxv

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
-const router=require('./Routes/user.routes');
-const fs=require('fs');
-const path=require('path');
-const globalError=require('./Controllers/error-controller/error.controller')
+const router = require('./Routes/user.routes');
+const fs = require('fs');
+const path = require('path');
+const globalError = require('./Controllers/error-controller/error.controller');
 const app = express();
+
 app.use(cookieParser());
 app.use(
   cors({
@@ -13,17 +14,21 @@ app.use(
     credentials: true,
   })
 );
-// Server side rendering react app
-app.use(express.static(path.join(__dirname,'./build')));
-app.get(/^(?!\/api\b).*|^\/?$/
-, (req, res) => {
-  console.log(req.url)
+
+// Serve static assets (including index.html) from the build directory
+app.use(express.static(path.join(__dirname, './build')));
+
+// Handle all routes on the server side and serve index.html
+app.get('*', (req, res) => {
+  console.log(req.url);
   const indexHtml = fs.readFileSync(path.join(__dirname, './build/index.html'), 'utf8');
   res.send(indexHtml);
 });
+
 app.use(express.json());
-app.use('/api/v1',router)
-app.options('*',cors())
-app.use(globalError)
+app.use('/api/v1', router);
+app.options('*', cors());
+app.use(globalError);
 app.use(express.urlencoded({ extended: true }));
+
 module.exports = app;
