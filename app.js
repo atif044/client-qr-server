d859cef2c2c57b4dbef0c21407bf443a16f7845a
atif044 <74341827+atif044@users.noreply.github.com>
2024-03-03 01:44:22 +0500

    finalize payment page

--- app.js
+++ app.js
@@ -15,33 +15,33 @@ app.use(
 );
 
 // Serve static assets (including index.html) from the build directory
-// app.use("/static",express.static(path.join(__dirname, './build/static'),{
-//   maxAge:86400000,
-// setHeaders: (res, path) => {
-//     if (path.endsWith('.js') || path.endsWith('.css') || path.endsWith('.png') || path.endsWith('.jpg') || path.endsWith('.jpeg')||
-// path.endsWith('.jsx')) {
-//       res.setHeader('Cache-Control', 'public, max-age=' + 86400000);
-//     }
-//   },
-// }));
-// app.get('/favicon.png', (req, res) => {
-//   const faviconPath = path.join(__dirname, './build/favicon.png');
-//   fs.readFile(faviconPath, (err, data) => {
-//     if (err) {
-//       console.error('Error reading favicon file:', err);
-//       res.status(404).end();
-//     } else {
-//       res.setHeader('Content-Type', 'image/x-icon');
-//       res.send(data);
-//     }
-//   });
-// });
-// // Handle all routes on the server side and serve index.html
-// app.get(/^(?!\/api\b).*|^\/?$/
-// , (req, res) => {
-//   const indexHtml = fs.readFileSync(path.join(__dirname, './build/index.html'), 'utf8');
-//   res.send(indexHtml);
-// });
+app.use("/static",express.static(path.join(__dirname, './build/static'),{
+  maxAge:86400000,
+setHeaders: (res, path) => {
+    if (path.endsWith('.js') || path.endsWith('.css') || path.endsWith('.png') || path.endsWith('.jpg') || path.endsWith('.jpeg')||
+path.endsWith('.jsx')) {
+      res.setHeader('Cache-Control', 'public, max-age=' + 86400000);
+    }
+  },
+}));
+app.get('/favicon.png', (req, res) => {
+  const faviconPath = path.join(__dirname, './build/favicon.png');
+  fs.readFile(faviconPath, (err, data) => {
+    if (err) {
+      console.error('Error reading favicon file:', err);
+      res.status(404).end();
+    } else {
+      res.setHeader('Content-Type', 'image/x-icon');
+      res.send(data);
+    }
+  });
+});
+// Handle all routes on the server side and serve index.html
+app.get(/^(?!\/api\b).*|^\/?$/
+, (req, res) => {
+  const indexHtml = fs.readFileSync(path.join(__dirname, './build/index.html'), 'utf8');
+  res.send(indexHtml);
+});
 app.use(express.json());
 app.use('/api/v1', router);
 app.options('*', cors());
